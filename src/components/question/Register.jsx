aaff28d40ce6d98ada9d7cb2e6b5fda74261ac27
Aman <kratos847473@gmail.com>
2024-08-24 18:44:29 +0530

    done

--- src/components/question/Register.jsx
+++ src/components/question/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -11,20 +11,27 @@ import {
   MDBIcon,
   MDBCheckbox
 } from 'mdb-react-ui-kit';
-
 import { saveStudent } from "../../../utils/StudentService";
+import FadeTransition from './FadeTransition'; // Import the FadeTransition component
+import './fadeTransition.css'; // Import the CSS file
+
 function Register() {
   const [student, setStudent] = useState({
     full_name: '',
     email: '',
     prn_no: '',
     roll_no: '',
-    password:'',
+    password: '',
     subscribe: false,
   });
 
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    setShowContent(true);
+  }, []);
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -42,28 +49,27 @@ function Register() {
     if (!student.prn_no) newErrors.prn_no = 'PRN is required';
     else if (student.prn_no <= 0) newErrors.prn_no = 'PRN should be a positive number';
     if (!student.roll_no) newErrors.roll_no = 'Roll Number is required';
-    if(!student.password) newErrors.password = 'Password is required';
-    else if(student.password.length<8)newErrors.password = 'Password must contain at least 8 character'
+    if (!student.password) newErrors.password = 'Password is required';
+    else if (student.password.length < 8) newErrors.password = 'Password must contain at least 8 characters';
     return newErrors;
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const validationErrors = validateFields();
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
     }
-  
+
     console.log('Submitting student data:', student);
-  
+
     setIsSubmitting(true);
     try {
       const response = await saveStudent(student);
       if (response && response.message === 'Student saved successfully') {
         alert('Registration successful!');
-        // Reset the form
         setStudent({
           full_name: '',
           email: '',
@@ -83,106 +89,108 @@ function Register() {
       setIsSubmitting(false);
     }
   };
-  
+
   return (
     <MDBContainer fluid>
-      <MDBCard className='text-black m-5' style={{ borderRadius: '25px' }}>
-        <MDBCardBody>
-          <MDBRow>
-            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
-              <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Student Sign Up</p>
+      <FadeTransition in={showContent}>
+        <MDBCard className='text-black m-5' style={{ borderRadius: '25px' }}>
+          <MDBCardBody>
+            <MDBRow>
+              <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Student Sign Up</p>
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="user" size='lg' className="me-3" />
-                <MDBInput
-                  label='Your Name'
-                  id='form1'
-                  type='text'
-                  name='full_name'
-                  value={student.full_name}
-                  onChange={handleChange}
-                  className={`w-100 ${errors.full_name ? 'border border-danger' : ''}`}
-                />
-              </div>
-              {errors.full_name && <p className="text-danger">{errors.full_name}</p>}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="user" size='lg' className="me-3" />
+                  <MDBInput
+                    label='Your Name'
+                    id='form1'
+                    type='text'
+                    name='full_name'
+                    value={student.full_name}
+                    onChange={handleChange}
+                    className={`w-100 ${errors.full_name ? 'border border-danger' : ''}`}
+                  />
+                </div>
+                {errors.full_name && <p className="text-danger">{errors.full_name}</p>}
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="envelope" size='lg' className="me-3" />
-                <MDBInput
-                  label='Your Email'
-                  id='form2'
-                  type='email'
-                  name='email'
-                  value={student.email}
-                  onChange={handleChange}
-                  className={`${errors.email ? 'border border-danger' : ''}`}
-                />
-              </div>
-              {errors.email && <p className="text-danger">{errors.email}</p>}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="envelope" size='lg' className="me-3" />
+                  <MDBInput
+                    label='Your Email'
+                    id='form2'
+                    type='email'
+                    name='email'
+                    value={student.email}
+                    onChange={handleChange}
+                    className={`${errors.email ? 'border border-danger' : ''}`}
+                  />
+                </div>
+                {errors.email && <p className="text-danger">{errors.email}</p>}
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="id-card" size='lg' className="me-3" />
-                <MDBInput
-                  label='PRN Number'
-                  id='form3'
-                  type='number'
-                  name='prn_no'
-                  value={student.prn_no}
-                  onChange={handleChange}
-                  className={`${errors.prn_no ? 'border border-danger' : ''}`}
-                />
-              </div>
-              {errors.prn_no && <p className="text-danger">{errors.prn_no}</p>}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="id-card" size='lg' className="me-3" />
+                  <MDBInput
+                    label='PRN Number'
+                    id='form3'
+                    type='number'
+                    name='prn_no'
+                    value={student.prn_no}
+                    onChange={handleChange}
+                    className={`${errors.prn_no ? 'border border-danger' : ''}`}
+                  />
+                </div>
+                {errors.prn_no && <p className="text-danger">{errors.prn_no}</p>}
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="address-card" size='lg' className="me-3" />
-                <MDBInput
-                  label='Roll Number'
-                  id='form4'
-                  type='text'
-                  name='roll_no'
-                  value={student.roll_no}
-                  onChange={handleChange}
-                  className={`${errors.roll_no ? 'border border-danger' : ''}`}
-                />
-              </div>
-              {errors.roll_no && <p className="text-danger">{errors.roll_no}</p>}
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="address-card" size='lg' className="me-3" />
+                  <MDBInput
+                    label='Roll Number'
+                    id='form4'
+                    type='text'
+                    name='roll_no'
+                    value={student.roll_no}
+                    onChange={handleChange}
+                    className={`${errors.roll_no ? 'border border-danger' : ''}`}
+                  />
+                </div>
+                {errors.roll_no && <p className="text-danger">{errors.roll_no}</p>}
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="address-card" size='lg' className="me-3" />
-                <MDBInput
-                  label='Password'
-                  id='form5'
-                  type='text'
-                  name='password'
-                  value={student.password}
-                  onChange={handleChange}
-                  className={`${errors.password ? 'border border-danger' : ''}`}
-                />
-              </div>
-              {errors.password && <p className="text-danger">{errors.password}</p>}
-              <div className='mb-4'>
-                <MDBCheckbox
-                  name='subscribe'
-                  value={student.subscribe}
-                  checked={student.subscribe}
-                  onChange={handleChange}
-                  id='flexCheckDefault'
-                  label='Accept All Terms And Conditions'
-                />
-              </div>
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="lock" size='lg' className="me-3" />
+                  <MDBInput
+                    label='Password'
+                    id='form5'
+                    type='password'
+                    name='password'
+                    value={student.password}
+                    onChange={handleChange}
+                    className={`${errors.password ? 'border border-danger' : ''}`}
+                  />
+                </div>
+                {errors.password && <p className="text-danger">{errors.password}</p>}
+                <div className='mb-4'>
+                  <MDBCheckbox
+                    name='subscribe'
+                    value={student.subscribe}
+                    checked={student.subscribe}
+                    onChange={handleChange}
+                    id='flexCheckDefault'
+                    label='Accept All Terms And Conditions'
+                  />
+                </div>
 
-              <MDBBtn className='mb-4' size='lg' onClick={handleSubmit} disabled={isSubmitting}>
-                {isSubmitting ? 'Submitting...' : 'Register'}
-              </MDBBtn>
-            </MDBCol>
+                <MDBBtn className='mb-4' size='lg' onClick={handleSubmit} disabled={isSubmitting}>
+                  {isSubmitting ? 'Submitting...' : 'Register'}
+                </MDBBtn>
+              </MDBCol>
 
-            <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-              <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
-            </MDBCol>
-          </MDBRow>
-        </MDBCardBody>
-      </MDBCard>
+              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
+              </MDBCol>
+            </MDBRow>
+          </MDBCardBody>
+        </MDBCard>
+      </FadeTransition>
     </MDBContainer>
   );
 }
